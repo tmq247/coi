49ade2780df9f951693d9782b847147e619fbe34
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-09-26 17:25:24 +0700

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -28,10 +28,10 @@ from config import BANNED_USERS, lyrical
 @app.on_message(
     filters.command(
         [
-            "play",
-            "vplay",
-            "cplay",
-            "cvplay",
+            "phat",
+            "vphat",
+            "cphat",
+            "cvphat",
             "playforce",
             "vplayforce",
             "cplayforce",
