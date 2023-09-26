c2dfb4e8c65df726b4b2ba4d7f615c6276611eb2
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-09-27 02:38:50 +0700

    Update channel.py

--- AnonXMusic/plugins/play/channel.py
+++ AnonXMusic/plugins/play/channel.py
@@ -39,7 +39,7 @@ async def playmode_(client, message: Message, _):
             async for user in app.get_chat_members(
                 chat.id, filter=ChatMembersFilter.ADMINISTRATORS
             ):
-                if user.status == SUDOERS:
+                if SUDOERS:
                     cusn = user.user.username
                     crid = user.user.id
         except:
