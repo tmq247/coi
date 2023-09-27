19f346d990b7c27dfdcbebbe0ae0be38b9fe2c9f
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-09-28 00:32:47 +0700

    Update queue.py

--- AnonXMusic/plugins/tools/queue.py
+++ AnonXMusic/plugins/tools/queue.py
@@ -36,7 +36,7 @@ def get_duration(playing):
 
 
 @app.on_message(
-    filters.command(["queue", "cqueue", "player", "cplayer", "playing", "cplaying"])
+    filters.command(["dscho", "cdscho", "nguoimo", "cnguoimo", "dangmo", "cdangmo"])
     & filters.group
     & ~BANNED_USERS
 )
