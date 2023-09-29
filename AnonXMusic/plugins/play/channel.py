32c1d0d6a4cb6695eb3ae155eed2ba01fa2a6a7c
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-09-29 10:54:48 +0700

    Update channel.py

--- AnonXMusic/plugins/play/channel.py
+++ AnonXMusic/plugins/play/channel.py
@@ -38,7 +38,7 @@ async def playmode_(client, message: Message, _):
             return await message.reply_text(_["cplay_5"])
         try:
             async for user in assistants.get_chat_members(
-                chat.id, filter=ChatMembersFilter.RECENT#ADMINISTRATORS
+                chat.id, filter=ChatMembersFilter.ADMINISTRATORS
             ):
                 if user.status == ChatMemberStatus.MEMBER:    #ADMINISTRATOR:
                     cusn = message.from_user.username#user.user.username
